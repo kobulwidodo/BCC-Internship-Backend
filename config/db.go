c15b5f1aae493cb80dcac0f151accc3c855082eb
kobulwidodo <fadhilhan01@gmail.com>
2021-03-02 22:06:01 +0700

    Add DB config at .env

--- config/db.go
+++ config/db.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"bengkel/entity"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,7 +12,7 @@ var DB *gorm.DB
 
 func InitDB() {
 	var err error
-	dsn := "root:@tcp(127.0.0.1:3306)/gobengkel?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := os.Getenv("DB_USER")+":"+os.Getenv("DB_PASS")+"@tcp(127.0.0.1:3306)/"+os.Getenv("DB_NAME")+"?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
